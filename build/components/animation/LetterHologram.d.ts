33cf523e29689c9a790efa0279f2543e82732c51
Nekzus <85507589+Nekzus@users.noreply.github.com>
2024-01-25 23:48:10 -0300

    fix(bug-fix): bug fix semantic release version

--- build/components/animation/LetterHologram.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { CSSProperties, ReactNode } from "react";
-interface RandomBgProps {
-    children: ReactNode;
-    className?: string;
-    colorClassName?: "blueColor" | "greenColor" | "pinkColor" | "orangeColor" | "darkColor" | "lightColor" | "defaultColor";
-    style?: CSSProperties;
-}
-declare const HoloBg: React.FC<RandomBgProps>;
-export default HoloBg;
--- build/components/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as HoloBg } from "./animation";
